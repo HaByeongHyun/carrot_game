f032b179e8b83b0b6b334c9a8b854daf9db59257
hakim <the9616@naver.com>
2021-04-23 06:34:57 +0900

    practice

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('pop-up__refresh');
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -32,31 +36,19 @@ function startGame() {
 }
 
 function stopGame() {
-  hiddenBtn();
-  replayPopup();
+  hideGameButton();
+  showPopupWithText('REPLAY?');
   stopGameTimer();
-}
-
-function hiddenBtn() {
-  gameBtn.style.background = 'transparent';
-  gameBtn.style.border = 'none';
-}
-
-function replayPopup() {
-  const popUp = document.querySelector('.pop-up--hide');
-  popUp.style.display = 'block';
-}
-
-function stopGameTimer() {
-  timer = setInterval(() => {
-    clearInterval(timer);
-  });
 }
 
 function showStopButton() {
   const icon = gameBtn.querySelector('.fa-play');
   icon.classList.add('fa-stop');
   icon.classList.remove('fa-play');
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = 'hidden';
 }
 
 function showTimeAndScore() {
@@ -76,11 +68,25 @@ function startGameTimer() {
   }, 1000);
 }
 
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
 function updateTimerText(time) {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   gameTimer.innerText = `${minutes}:${seconds}`;
 }
+
+function showPopupWithText(text) {
+  popUpText.innerText = text;
+  popUp.classList.remove('pop-up--hide');
+  popUpRefresh.addEventListener('click', () => {
+    console.log('log');
+  });
+}
+
+function restartGame() {}
 
 function initGame() {
   field.innerHTML = '';
